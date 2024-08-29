2d99d3d58b09e243ebda8bae7990a8e09bd0afa6
liam nelsen <lnelsen@ithaca.edu>
2024-08-29 14:27:42 -0400

    implemented doubleSize and it works

--- src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Rectangle.java
@@ -16,7 +16,8 @@ public class Rectangle {
     }
 
     public void doubleSize(){
-        throw new RuntimeException("Not implemented yet");
+        this.length *= 2;
+        this.width *= 2;
     }
 
     public double longestLineWithin(){
