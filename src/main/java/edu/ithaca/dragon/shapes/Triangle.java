301ab9212be05ef80b6b1fe06a194c274a20972b
liam nelsen <lnelsen@ithaca.edu>
2024-08-30 12:20:37 -0400

    created method signatures for triangle

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,23 @@
+package edu.ithaca.dragon.shapes;
+
+public class Triangle {
+    double base;
+    double height;
+
+    public Triangle(double base, double height){
+        this.base = base;
+        this.height = height;
+    }
+
+    public double calcArea(){
+        
+    }
+
+    public void doubleSize(){
+
+    }
+
+    public doule longestLineWithin(){
+
+    }
+}
